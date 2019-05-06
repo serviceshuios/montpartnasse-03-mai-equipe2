d30b14dc71e781cadb39f1535640eba9067ea161
Imene Souei <soueiimen@yahoo.fr>
2019-05-06 12:38:04 +0200

    Controller

---
+++ src/main/java/com/infotel/MavenSpringDataMvc/AerienneController.java
@@ -0,0 +1,55 @@
+package com.infotel.MavenSpringDataMvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.infotel.MavenSpringDataMvc.metier.Aerienne;
+
+
+@Controller
+public class AerienneController {
+	
+	@Autowired
+    private Iservice service;
+
+    @RequestMapping(value = "/aerienne", method = RequestMethod.GET)
+    public String lister(Model model) {
+        model.addAttribute("aerienne", new Aerienne());
+        model.addAttribute("aerienne", service.findAllAerienne());
+        return "aeriennes";
+    }
+
+    @RequestMapping(value = "/saveAerienne")
+    public String save(Aerienne aerienne,  Model model) {
+        if (aerienne.getIdCargaison() == 0) {
+            service.ajouterAerienne(aerienne);
+            model.addAttribute("aerienne", new Aerienne());
+            model.addAttribute("aeriennes", service.findAllAerienne());
+            return "aeriennes";
+        } else {
+            service.modifierAerienne(aerienne);
+            model.addAttribute("aerienne", new Aerienne());
+            model.addAttribute("aeriennes", service.findAllAerienne());
+            return "aeriennes";
+        }
+    }
+    @RequestMapping(value = "/deleteAerienne")
+    public String delete(@RequestParam int idCargaison, Model model) {
+        service.supprimerAerienne(idCargaison);
+        model.addAttribute("aerienne", new Aerienne());
+        model.addAttribute("aeriennes", service.findAllAerienne());
+        return "aeriennes";
+    }
+
+    @RequestMapping(value = "/editAerienne")
+    public String edit(@RequestParam int idCargaison, Model model) {
+        model.addAttribute("aerienne", service.getAerienne(idCargaison));
+        model.addAttribute("aeriennes", service.findAllAerienne());
+        return "aeriennes";
+    }
+
+}
