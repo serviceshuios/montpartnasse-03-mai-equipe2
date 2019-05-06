a1c0a5292a75bbf2af9bf7d3a05927ba59e8acfe
IN-MP-012 <zakarya.baroudi@gmail.com>
2019-05-06 14:17:23 +0200

    repo

--- src/main/java/com/infotel/MavenSpringDataMvc/dao/RoutiereRepository.java
+++ src/main/java/com/infotel/MavenSpringDataMvc/dao/RoutiereRepository.java
@@ -2,8 +2,8 @@ package com.infotel.MavenSpringDataMvc.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.infotel.MavenSpringDataMvc.metier.Cargaison;
+import com.infotel.MavenSpringDataMvc.metier.Routiere;
 
-public interface RoutiereRepository extends JpaRepository <Cargaison, Integer>{
+public interface RoutiereRepository extends JpaRepository <Routiere, Integer>{
 
 }
